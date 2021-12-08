7639eb47544ac5fec7ac785cb1d04ec77e45004b
Adam Izydorczyk <adamflorianizydorczyk@gmail.com>
2021-12-08 19:02:01 +0100

    Day 3 part 1 correction and part 2

--- src/main/scala/Ex3.scala
+++ src/main/scala/Ex3.scala
@@ -1,19 +1,19 @@
-
+import scala.annotation.tailrec
 
 object Ex3 extends App {
   val report = PuzzleInputDownloader.download("https://adventofcode.com/2021/day/3/input")
 
-  private def partOne = {
-    val numbersWithIndexes = report.flatMap(_.split("")
-      .map(_.toInt).zipWithIndex)
+  private def partOne: Int = {
+    val numbersWithIndexesByIndexes = report.flatMap(_.split("").zipWithIndex)
+      .groupBy(_._2)
 
-    val sumsByIndexes = numbersWithIndexes.groupBy(_._2)
-      .view.mapValues(_.map(_._1).sum)
+    val commonValueOnIndexByIndex = numbersWithIndexesByIndexes.view
+      .mapValues(_.groupMapReduce(identity)(_ => 1)(_ + _).maxBy(_._2)._1)
 
-    val binaryGamma = sumsByIndexes.toList.sortBy(_._1)
-      .map(_._2.toDouble / report.length)
-      .map(math.round)
-      .map(_.toInt).mkString
+    val binaryGamma = commonValueOnIndexByIndex.toList
+      .sortBy(_._1)
+      .map(_._2._1)
+      .mkString
 
     val binaryEpsilon = binaryGamma.map {
       case '1' => true
@@ -29,5 +29,37 @@ object Ex3 extends App {
 
     gamma * epsilon
   }
+
   println(partOne)
+
+  private def partTwo: Int = {
+    val bits: Seq[Array[Int]] = report.map(_.split("")
+      .map(_.toInt))
+
+    @tailrec
+    def calculateRate(commonCondition: (Int, Int) => Int,
+                      bits: Seq[Array[Int]],
+                      position: Int): Int = {
+      if (bits.length <= 1) {
+        return Integer.parseInt(bits.head.mkString, 2)
+      }
+
+      val bitsOnPosition = bits.map(_ (position))
+      val countByBit = bitsOnPosition.groupBy(identity).view
+        .mapValues(_.size)
+      val zeros = countByBit.getOrElse(0, 0)
+      val ones = countByBit.getOrElse(1, 0)
+      val common = commonCondition.apply(ones, zeros)
+
+      calculateRate(commonCondition, bits.filter(_ (position) == common), position + 1)
+    }
+
+    val o2Rate = calculateRate((ones, zeros) => if (ones >= zeros) 1 else 0, bits, 0)
+    val co2Rate = calculateRate((ones, zeros) => if (ones >= zeros) 0 else 1, bits, 0)
+
+    o2Rate * co2Rate
+  }
+
+  println(partTwo)
+
 }
